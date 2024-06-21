3164865833c3976a52c0a94d79e61a8f77b0b3ac
kelvinsibuta254 <kelvinsibuta254@gmail.com>
2024-06-21 08:27:49 -0700

    Added a python script that performs basic arithmetic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,9 +1,9 @@
 #!/bin/bash
 number1 = 10
 number2 = 5
-Addition = number1 + number2
+addition = number1 + number2
 difference = number1 - number2
 product = number1 * number2
-print("Addition")
-print("difference")
-print("product")
+print("addition")
+print("subtraction")
+print("multiplication")
