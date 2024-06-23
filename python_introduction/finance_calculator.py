93770b6b5e6d8aacac809130807f9a5d56942634
kelvinsibuta254 <kelvinsibuta254@gmail.com>
2024-06-23 07:28:26 -0700

    added a script that will calculate and provide feedback on a user's monthly savings and potential future savings

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,7 @@
+#!/bin/bash
+monthly_income = int(input("Enter monthly income:"))
+total_monthly_expenses = int(input("Enter your total monthly expenses:"))
+monthly_savings = (monthly_income - total_monthly_expenses)
+print(f"Your monthly savings are {monthly_savings}")
+projected_savings = (monthly_savings * 12 + (monthly_savings * 12 * 0.05))
+print(f"Projected savings after one year, with interest, is: {projected_savings}")
