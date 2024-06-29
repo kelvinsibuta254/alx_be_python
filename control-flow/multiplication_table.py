803fa0ee3949930deaa01f1a68b01447dd91e90d
kelvinsibuta254 <kelvinsibuta254@gmail.com>
2024-06-29 04:44:10 -0700

    Added a python script that asks the user to enter a number, then use a for loop to print the multiplication table for that number

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -2,4 +2,4 @@
 number = input("Enter a number to see its multiplication table: ")
 for i in range(1, 11):
     result = number * i
-    print(number, "*", i, "=", result)
+    print(f"{number} * {i} = {result}")
