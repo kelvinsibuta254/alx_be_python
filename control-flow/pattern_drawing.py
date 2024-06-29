e6fc37ff1b037f184485bea9adc65142419921b1
kelvinsibuta254 <kelvinsibuta254@gmail.com>
2024-06-29 09:16:04 -0700

    added pattern drawing

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,12 @@
+#!/bun/bash
+rows = input("Enter the size of the pattern")
+i = 0
+while i <= 4:
+    for i in range(1, 5):
+        j = 0
+        while j <= 4:
+            for j in range(1, 5):
+                print("*", end="")
+                j += 1
+            i += 1
+        print()
