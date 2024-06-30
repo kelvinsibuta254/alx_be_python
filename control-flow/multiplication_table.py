e2f852cef2b9dae8f7ec390649772b63269bbd25
kelvinsibuta254 <kelvinsibuta254@gmail.com>
2024-06-30 03:17:20 -0700

    Added a multiplication table that will ask the user to enter a number, then user a for loop to print the multiplication table for that number

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,5 +1,5 @@
 #!/bin/bash
-number = input("Enter a number to see its multiplication table: ")
+number = int(input("Enter a number to see its multiplication table: "))
 for i in range(1, 11):
     result = number * i
     print(f"{number} * {i} = {result}")
