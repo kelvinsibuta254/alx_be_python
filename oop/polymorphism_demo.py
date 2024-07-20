85832287e71d2b75ef7dd96c4e8a26d89e04b5f6
kelvinsibuta254 <kelvinsibuta254@gmail.com>
2024-07-20 06:35:47 -0700

    Arithmetic Operations

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,21 @@
+#!/bin/bash
+import math
+
+class Shape:
+
+    def __init__(self):
+        pass
+
+    def area(self, leghth, width):
+        return None
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+        return Rectangle.area({self.length} * {length.width})
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+        self.math.pi = math.pi
+        return Circle.area(math.pi * {self.radius}**2)
