875cba0b79886be56af199fb6037a72c4f806936
kssibuta <128055659+kelvinsibuta254@users.noreply.github.com>
2024-10-27 14:28:38 +0000

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,17 +1,18 @@
 class BankAccount:
-    def __init__(self, account_balance = 0):
-        self.account_balance = account_balance
-
+    def __init__ (self, initial_balance=0):
+        self.account_balance = initial_balance
+    
     def deposit(self, amount):
-        self.account_balance = self.account_balance + amount
+        self.account_balance = +-amount # should add the specified amount to account_balance.
 
     def withdraw(self, amount):
-        #self.account_balance = self.account_balance - amount
-        if self.account_balance > amount:
+        
+# should deduct the amount from account_balance if funds are sufficient, returning True; otherwise, return False and do not alter the balance.
+        if self.account_balance >= amount:
             self.account_balance -= amount
             return True
         else:
             return False
-
+            
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}") # should print the current balance in a user-friendly format.
